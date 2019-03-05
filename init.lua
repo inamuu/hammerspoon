7a337721172f8537563eb84f5dfa96d809d16f3c
inamuu <8310973+inamuu@users.noreply.github.com>
2019-03-05 22:53:10 +0900

    :up: backup

--- init.lua
+++ init.lua
@@ -1,3 +1,20 @@
+local function keyCode(key, modifiers)
+   modifiers = modifiers or {}
+   return function()
+      hs.eventtap.event.newKeyEvent(modifiers, string.lower(key), true):post()
+      hs.timer.usleep(1000)
+      hs.eventtap.event.newKeyEvent(modifiers, string.lower(key), false):post()      
+   end
+end
+
+local function remapKey(modifiers, key, keyCode)
+   hs.hotkey.bind(modifiers, key, keyCode, nil, keyCode)
+end
+
+
+-- iot button
+-- local blbutton = 0x0100
+
 -- Hello World
 hs.hotkey.bind({"cmd", "alt", "ctrl"}, "W", function()
   hs.alert.show("Hello World")
